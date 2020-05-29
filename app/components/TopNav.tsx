cdae132e2ce61636493c6ba05cba285db32669f5
Nick <nrcrast@gmail.com>
2020-05-29 16:52:41 -0400

    Add donate button

--- app/components/TopNav.tsx
+++ app/components/TopNav.tsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import {shell} from 'electron';
 import { Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
 import routes from '../constants/routes.json';
 
 export default function TopNav() {
+  function openDonate(e) {
+    e.preventDefault();
+    let link = e.target.href;
+    shell.openExternal(link);
+  }
   return (
     <Navbar variant="dark" className="rounded shadow main-navbar">
       <Navbar.Brand>Icon Toolbox</Navbar.Brand>
@@ -24,6 +31,9 @@ export default function TopNav() {
         <Nav.Link as={Link} to={routes.CREATE}>
           Create
         </Nav.Link>
+      </Nav>
+      <Nav className="ml-auto">
+        <Button variant="secondary" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=JWX35VP4RXA7A&source=url" onClick={openDonate}>Donate</Button>
       </Nav>
     </Navbar>
   );
