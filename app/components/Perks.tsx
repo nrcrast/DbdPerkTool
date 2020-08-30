c22601ab5844e7559e2d4db927dc80be64445d40
Nick <nrcrast@gmail.com>
2020-08-30 16:14:37 -0400

    Remove default pack from main icon list

--- app/components/Perks.tsx
+++ app/components/Perks.tsx
@@ -12,30 +12,29 @@ export default class Perks extends Component<MyProps, MyState> {
   }
 
   fromPacksBuildCards(packs, opts) {
-    return packs.map(pack => {
-      return (
-        <PerkPack
-          viewMode={opts.viewMode}
-          onError={opts.onError}
-          onInstallComplete={opts.onInstallComplete}
-          meta={pack}
-          id={pack.id}
-          downloads={pack.downloads}
-          setFilter={opts.onSetFilter}
-          onAuthorClick={(author: string) => {
-            opts.onAuthorClick(author);
-          }}
-        />
-      );
-    });
+    return packs
+      .filter(pack => pack.id !== 'Dead-By-Daylight-Default-Icons')
+      .map(pack => {
+        return (
+          <PerkPack
+            viewMode={opts.viewMode}
+            onError={opts.onError}
+            onInstallComplete={opts.onInstallComplete}
+            meta={pack}
+            id={pack.id}
+            downloads={pack.downloads}
+            setFilter={opts.onSetFilter}
+            onAuthorClick={(author: string) => {
+              opts.onAuthorClick(author);
+            }}
+          />
+        );
+      });
   }
 
   render() {
     return (
-      <PackDisplay
-        packQuery={{}}
-        cardBuilder={this.fromPacksBuildCards}
-      />
+      <PackDisplay packQuery={{}} cardBuilder={this.fromPacksBuildCards} />
     );
   }
 }
